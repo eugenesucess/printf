611aebc195a6c08da0d486a782f8460b8701c0be
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-27 14:21:48 -0700

    contains all prototypes

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * struct modify - Struct for format
+ * @modifier: character which shows what to print
+ * @f: The function pointer
+ */
 
 typedef struct modify
 {
