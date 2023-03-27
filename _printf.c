567437c5062ebe2271a83dd1351c557e6522b34c
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-27 14:53:44 -0700

    prints

--- _printf.c
+++ _printf.c
@@ -3,33 +3,42 @@
 #include <stdarg.h>
 #include "main.h"
 
-
+/**
+ * functions -> get prototypes
+ * @format: va_lits
+ * Return: null
+ */
 int (*functions(const char *format))(va_list)
 {
-    unsigned int i = 0;
-    eachModifier fun_used[] ={
-        {"c", print_char},
-        {"s", print_string},
-        {"d", print_number},
-        {"i", print_number},
-/*        {"r", print_rev},*/
-        {"%", print_mod},
-/*        {"b", print_bin},*/
-/*        {"o", print_octal},*/
-        {"u", print_unsig},
-/*        {"x", print_x},*/
-        {NULL, NULL},
-    };
+	unsigned int i = 0;
+	eachModifier fun_used[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_number},
+		{"i", print_number},
+		/*        {"r", print_rev},*/
+		{"%", print_mod},
+		/*        {"b", print_bin},*/
+		/*        {"o", print_octal},*/
+		{"u", print_unsig},
+		/*        {"x", print_x},*/
+		{NULL, NULL},
+	};
 
-    while (fun_used[i].modifier)
+	while (fun_used[i].modifier)
 	{
 		if (fun_used[i].modifier[0] == (*format))
 			return (fun_used[i].f);
 		i++;
 	}
-    return (NULL);
+	return (NULL);
 }
 
+/**
+ * _printf -> print
+ * @format: va_list
+ * Return: int;
+ */
 
 int _printf(const char *format, ...)
 {
