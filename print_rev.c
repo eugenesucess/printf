0d8be7ac2efed8508cd68ecd3f90d6dadb9a72ca
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-27 14:59:04 -0700

    reverse string

--- print_rev.c
+++ print_rev.c
@@ -2,25 +2,30 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * print_rev -> reverse string
+ * @r: string
+ * Return: string
+ */
 
 int print_rev(va_list r)
 {
-    char *str;
-    int len = 0, print = 0;
+	char *str;
+	int len = 0, print = 0;
 
-    str = va_arg(r, char *);
-    if (str == NULL)
-        str = ")llun(";
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
 
-    while(str[len])
-    {
-        len++;
-    }
+	while (str[len])
+	{
+		len++;
+	}
 
 	for (len -= 1 ; len >= 0; len--)
-    {
-        _putchar(str[len]);
-        print++;
-    }
-    return (print);
+	{
+		_putchar(str[len]);
+		print++;
+	}
+	return (print);
 }
