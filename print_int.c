16f3a0c28aa93229ebf5c891157a3350e42b51ce
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-27 14:31:21 -0700

    print int

--- print_int.c
+++ print_int.c
@@ -1,61 +1,36 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+
+/**
+ * print_number -> print in
+ * @i: integer to print
+ * Return: int
+ */
+
 int print_number(va_list i)
 {
-    int num = va_arg(i, int);
-    int digit, reversed = 0;
+	int num = va_arg(i, int);
+	int digit, reversed = 0;
 
-    if (num < 0)
-    {
-        _putchar('-');
-        num *= -1;
-    }
-    while (num != 0)
-    {
-        digit = num % 10;
-        reversed = reversed * 10 + digit;
-        num /= 10;
-    }
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	while (num != 0)
+	{
+		digit = num % 10;
+		reversed = reversed * 10 + digit;
+		num /= 10;
+	}
 
-    while (reversed != 0)
-    {
-        digit = reversed % 10;
-        _putchar(digit + '0');
-        reversed /= 10;
-    }
-    return (num);
+	while (reversed != 0)
+	{
+		digit = reversed % 10;
+		_putchar(digit + '0');
+		reversed /= 10;
+	}
+	return (num);
 }
-/*
-int print_int(va_list i)
-{
-    int myInt;
-    myInt = va_arg(i, int);
 
-    if (myInt == 0)
-    {
-        _putchar('0');
-
-    }
-    if (myInt < 0)
-    {
-        _putchar('-'); //bring minus sign for negative numbers
-        myInt *= -1; //original negative number
-    }
-    if (myInt < 10)
-       _putchar(myInt + '0');
-
-    if (myInt >= 10)
-    {
-        int out;
-        myInt /= 10;
-        out = myInt;
-        _putchar(out + '0');
-        myInt %= 10;
-        out += myInt;
-        _putchar(out + '0');
-
-    }
-    return (myInt);
-}
-*/
