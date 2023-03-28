65a5fde9828544452f298a9abffe384dc2b86d9c
Bertrand Kwizera <44434029+Gudekwizera@users.noreply.github.com>
2023-03-28 20:55:52 +0200

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
+ #include "main.h"
+ #include <unistd.h>
 
-/**
- * _putchar - write character c to the stdout
- * @c: the character
- * Return: 1 on success and -1 otherwise
- */
+ /**
+  * _putchar -> print something
+  * @c: char
+  * Return: int
+  */
 
-int _putchar(char c)
+ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
